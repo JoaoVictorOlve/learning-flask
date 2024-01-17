3ad85178ca40a1c0a57e162a83037b32edbc96b3
JoaoVictorOlve <jvoliveira2005@gmail.com>
2024-01-17 18:06:08 -0300

    update

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
-from flask import Flask
+from flask import Flask, url_for, request, render_template
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/about')
-def about():
-    return 'The about page'
-
-@app.route('/about/<int:number>')
-def about(number):
-    return f'The about {number}'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('layout.html', name=name)
+    
