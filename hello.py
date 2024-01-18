cbc4897c15b9d7ef6363231149d532cc228f5315
JoaoVictorOlve <jvoliveira2005@gmail.com>
2024-01-18 17:28:05 -0300

    update

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, url_for, request, render_template, abort, redirect
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/hello/')
-@app.route('/hello/<name>')
-def hello(name=None):
-    return render_template('layout.html', name=name)
-    
+@app.route('/')
+def index():
+    return redirect(url_for('login'))
+
+@app.route('/login')
+def login():
+    abort(401)
